450846ffe07f7b7a1d5df8ee008e07d17cff1980
Julian EASTERLY <julianeasterly@gmail.com>
2018-09-30 18:56:56 +0200

    finished coef

--- src/weight_calculator.py
+++ src/weight_calculator.py
@@ -1,15 +1,14 @@
+import pprint
 from collections import OrderedDict
 from math import sqrt
 
 
 class WeightCalculator:
 
-    def __init__(self):
-        self._query_results = OrderedDict()
-
     def calculate_weights(self, doc_list, word_base, query):
         query_size = len(query.split(' '))
         unordered_results = {}
+
         for doc in doc_list:
             denominator = 0
             numerator = 0
@@ -22,11 +21,5 @@ class WeightCalculator:
             denominator *= query_size
             index = numerator / sqrt(denominator)
             unordered_results[index] = doc
-            self.__sort_results(unordered_results=unordered_results)
-
-        return self._query_results
-
-    def __sort_results(self, unordered_results):
-        for key in sorted(unordered_results, reverse=True):
-            print(key)
-            self._query_results[key] = key
+        pprint.pprint(unordered_results)
+        return OrderedDict(sorted(unordered_results.items(), reverse=True))
