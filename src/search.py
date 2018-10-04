4e44fe148915cf7e107d2ffb6b22a3802fe2a0b3
leymarie <leymarie@ecole.ensicaen.fr>
2018-10-04 15:51:57 +0200

    search class finished and tested

--- src/search.py
+++ src/search.py
@@ -6,33 +6,35 @@ Created on Sun Sep 30 16:52:31 2018
 """
 
 from src.indexer import Indexer
-from src.weight_calculator import WeightCalculator
+from src.relevence_calculator import RelevenceCalculator
 
 class Search :
     def __init__(self):
         self._indexer = Indexer()
-        self._doc_list = {}
         self._query = ""        
 
-    def __enter_query(self) : 
-        _query = input("Request : ")
-        print(_query)
+    def enter_query(self) : 
+        self._query = input("Input query : ")
         
-    def __list_salton(self) : 
-        _orderedDict = WeightCalculator.calculate_weights(self._doc_list, self.indexer.word_base, self._query)
-        WeightCalculator.__sort_results(_orderedDict)
+    def list_salton(self) : 
+        print("The results for your query \"", self._query, "\" : \n")
+        _orderedDict = RelevenceCalculator.calculate_salton_coefficient(self, self._indexer.doc_list, self._indexer.index(), self._query)
+        print("Coefficient list (ordered) : ")        
+        for item in _orderedDict : 
+            print(item)
     
-    #def __most_relevant_docs(self) :
-        #_doc_list = self._indexer.doc_list()
+#    def most_relevant_documents(self) :
+        
 
     def test_salton(self) :
-        _queryTest = "automne";
-        _index = Indexer();
-        _orderedDict = WeightCalculator.calculate_weights(self, _index.doc_list, _index._word_base, _queryTest)
-        WeightCalculator.__sort_results(_orderedDict)
-
+        _queryTest = "automne"
+        _index = Indexer()
+        _orderedDict = RelevenceCalculator.calculate_salton_coefficient(self, _index.doc_list, _index.index(), _queryTest)
+        for item in _orderedDict : 
+            print(item)
 
 
 if __name__ == '__main__':
     search = Search()
-    search.test_salton()
+    search.enter_query()
+    _list = search.list_salton()
